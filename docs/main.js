b7af3e24f05867b8cf1f52032460a4194453c2c4
JonathanMosa <jmosa0822@sdsu.edu>
2025-05-06 15:38:37 -0700

    Add files via upload

---
+++ docs/main.js
@@ -0,0 +1,9 @@
+// This file contains JavaScript code to handle interactions on the website.
+
+document.addEventListener('DOMContentLoaded', function() {
+    const githubButton = document.querySelector('button');
+    githubButton.addEventListener('click', function() {
+        window.open('https://github.com/JonathanMosa/Dino-lution/tree/main', '_blank');
+    });
+
+});
